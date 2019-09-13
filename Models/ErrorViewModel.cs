a0a96a324e7516817a87038f6f71b12e59a46762
genious420 <53940628+genious420@users.noreply.github.com>
2019-09-14 00:22:51 +0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SecondAssingnment.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
