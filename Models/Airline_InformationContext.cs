049c74bde16b7e53778994d0daa951e09498b678
genious420 <53940628+genious420@users.noreply.github.com>
2019-09-14 00:54:17 +0500

    Scaffold commad empliment

---
+++ Models/Airline_InformationContext.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace SecondAssingnment.Models
+{
+    public partial class Airline_InformationContext : DbContext
+    {
+        public Airline_InformationContext()
+        {
+        }
+
+        public Airline_InformationContext(DbContextOptions<Airline_InformationContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Airline> Airline { get; set; }
+        public virtual DbSet<Flight> Flight { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=DESKTOP-KBDTL9C;Database=Airline_Information;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.6-servicing-10079");
+
+            modelBuilder.Entity<Airline>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Country)
+                    .HasColumnName("country")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Name)
+                    .HasColumnName("name")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.ShortCode)
+                    .HasColumnName("short_code")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Status)
+                    .HasColumnName("status")
+                    .HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<Flight>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.AirlineId).HasColumnName("airline_id");
+
+                entity.Property(e => e.ArrivalDateTime)
+                    .HasColumnName("arrival_date_time")
+                    .HasColumnType("datetime");
+
+                entity.Property(e => e.DepartialDateTime)
+                    .HasColumnName("departial_date_time")
+                    .HasColumnType("datetime");
+
+                entity.Property(e => e.Destination)
+                    .HasColumnName("destination")
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Source)
+                    .HasColumnName("source")
+                    .HasMaxLength(50);
+            });
+        }
+    }
+}
