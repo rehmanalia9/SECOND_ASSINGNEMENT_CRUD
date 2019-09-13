5550b0fb7afa93fd3713c9e3279fef2d731d59c3
genious420 <53940628+genious420@users.noreply.github.com>
2019-09-14 01:13:18 +0500

    Database configration

--- Models/Airline_InformationContext.cs
+++ Models/Airline_InformationContext.cs
@@ -18,14 +18,14 @@ namespace SecondAssingnment.Models
         public virtual DbSet<Airline> Airline { get; set; }
         public virtual DbSet<Flight> Flight { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=DESKTOP-KBDTL9C;Database=Airline_Information;Trusted_Connection=True;");
-            }
-        }
+//        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//        {
+//            if (!optionsBuilder.IsConfigured)
+//            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+//                optionsBuilder.UseSqlServer("Server=DESKTOP-KBDTL9C;Database=Airline_Information;Trusted_Connection=True;");
+//            }
+//        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
--- Startup.cs
+++ Startup.cs
@@ -7,8 +7,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using SecondAssingnment.Models;
 
 namespace SecondAssingnment
 {
@@ -31,6 +33,9 @@ namespace SecondAssingnment
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
+            services.AddDbContext<Airline_InformationContext>(options =>
+               options.UseSqlServer(
+                   Configuration.GetConnectionString("GENI")));
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
